997b6a23e011c3a3af7201eb8f1580192c4afb26
aristokratos <oluwatobiloba.onawale@decagon.dev>
2023-01-25 16:54:13 +0100

    Updated

--- WeatherApi_AuthnAutho/Services/UserService/UserService.cs
+++ WeatherApi_AuthnAutho/Services/UserService/UserService.cs
@@ -5,18 +5,22 @@ namespace WeatherApi_AuthnAutho.Services.UserService
 {
     public class UserService : IUserService
     {
+        //access to httpcontext
         private readonly IHttpContextAccessor _httpContextAccessor;
 
         public UserService(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
         }
+        //Getting name of the authorized user
 
         public string GetMyName()
         {
+            //checking if the httpcontext is not null
             var result = string.Empty;
             if (_httpContextAccessor.HttpContext != null)
             {
+                //getting name of the user from claims
                 result = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
             }
             return result;
